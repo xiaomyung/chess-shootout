34c2942c5c09f5a7128e521391c58b494306a804
Xiao Myung <xiaomyung.dev@gmail.com>
2026-05-12 23:07:08 +0300

    backend: lift shared shape constants + on_board into pieces/utils

--- backend/pieces.py
+++ backend/pieces.py
@@ -37,3 +37,37 @@ BACK_RANK = [
     PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, PieceType.QUEEN,
     PieceType.KING, PieceType.BISHOP, PieceType.KNIGHT, PieceType.ROOK,
 ]
+
+
+KNIGHT_OFFSETS = [
+    (-2, -1), (-2, 1), (-1, -2), (-1, 2),
+    (1, -2), (1, 2), (2, -1), (2, 1),
+]
+
+KING_OFFSETS = [
+    (-1, -1), (-1, 0), (-1, 1), (0, -1),
+    (0, 1), (1, -1), (1, 0), (1, 1),
+]
+
+BISHOP_DIRECTIONS = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+ROOK_DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+QUEEN_DIRECTIONS = BISHOP_DIRECTIONS + ROOK_DIRECTIONS
+
+WHITE_KING_HOME_ROW = 7
+BLACK_KING_HOME_ROW = 0
+KING_HOME_COL = 4
+CASTLE_TARGET_COLS = (2, 6)
+WHITE_PAWN_START_ROW = 6
+BLACK_PAWN_START_ROW = 1
+
+
+def king_home_row(color):
+    return WHITE_KING_HOME_ROW if color == PieceColor.WHITE else BLACK_KING_HOME_ROW
+
+
+def pawn_start_row(color):
+    return WHITE_PAWN_START_ROW if color == PieceColor.WHITE else BLACK_PAWN_START_ROW
+
+
+def pawn_forward(color):
+    return -1 if color == PieceColor.WHITE else 1
